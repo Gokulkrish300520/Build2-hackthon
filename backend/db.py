7e8156b2d40116551a91fda389f263f218a24af6
Gokulkrish300520 <gokul.jayakumar2005@gmail.com>
2026-02-19 04:24:56 +0530

    error has been fixed

--- backend/db.py
+++ backend/db.py
@@ -1,7 +1,7 @@
 
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from .models import Base
+from models import Base
 import os
 
 # Use environment variable for Railway PostgreSQL
